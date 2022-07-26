bbc4146882757ebd27c21c00c3b3de79dc951e7c
Liam DeBeasi <liamdebeasi@icloud.com>
2022-07-26 21:16:42 +0000

    chore(): add repros

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<ion-button>Click me</ion-button>
+<ion-button>Ionic button</ion-button>
+<sl-button>Shoelace Button</sl-button>
+<wc-button>Native WC Button</wc-button>
